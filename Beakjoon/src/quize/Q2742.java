0477fc46a8a947ec9fdca187a169fdc31a48d79b
hedgehog9 <redpwon@gmail.com>
2022-11-15 12:45:05 +0900

    백준 2742 문제 풀이

---
+++ Beakjoon/src/quize/Q2742.java
@@ -0,0 +1,18 @@
+package quize;
+
+import java.util.Scanner;
+
+public class Q2742 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i =n; i>0; i--) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
